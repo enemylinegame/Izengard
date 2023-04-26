c22060ebd8664327fdb96df96cf03f9c8bc234e5
Mikhail Nemtinov <86906070+NeMikhail@users.noreply.github.com>
2023-04-27 01:13:27 +0300

    Change ResourceView

--- Assets/Code/TileSystem/Views/ResourceView.cs
+++ Assets/Code/TileSystem/Views/ResourceView.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -6,29 +7,40 @@ namespace Code.TileSystem
 {
     public class ResourceView : MonoBehaviour
     {
-        [SerializeField] private TMP_Text _resourceValue;
         [SerializeField] private TMP_Text _resourceText;
+        [SerializeField] private TMP_Text _resourceCurrentValue;
+        [SerializeField] private TMP_Text _resourceMaxValue;
 
         [SerializeField] private Button _resourceAddButton;
         [SerializeField] private Button _resourceRemoveButton;
 
-        public string ResourceValueString 
+        public string ResourceCurrentValueString
         {
-            get { return _resourceValue.text; }
-            set { _resourceValue.text = value; }
+            get { return _resourceCurrentValue.text; }
+            set { _resourceCurrentValue.text = value; }
         }
-        public string ResourceTextString 
+        public int ResourceCurrentValueInt
+        {
+            get { return Convert.ToInt32(_resourceCurrentValue.text); }
+        }
+        public string ResourceTextString
         {
             get { return _resourceText.text; }
             set { _resourceText.text = value; }
         }
-        public Button ResourceAddButton 
-        { 
+        public Button ResourceAddButton
+        {
             get { return _resourceAddButton; }
         }
-        public Button ResourceRemoveButton 
+        public Button ResourceRemoveButton
         {
             get { return _resourceRemoveButton; }
+        }
+
+        public void InitViewData()
+        {
+            _resourceCurrentValue.text = "0";
+            _resourceMaxValue.text = "5";
         }
     }
 }
