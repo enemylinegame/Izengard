c33bc963f37970eb2aefec1f7b3117205b08c975
Mikhail Nemtinov <86906070+NeMikhail@users.noreply.github.com>
2023-04-26 16:03:33 +0300

    AddResourceViewPropertyToTileUIView

--- Assets/Code/TileSystem/Views/TileUIView.cs
+++ Assets/Code/TileSystem/Views/TileUIView.cs
@@ -13,6 +13,7 @@ namespace Code.TileSystem
         [SerializeField] private Button _upgrade;
         [SerializeField] private Button _destroy;
         [SerializeField] private TMP_Text _unitMax;
+        [SerializeField] private TileResourcesView _tileResourcesView;
 
         public Image Icon => _icon;
         public TMP_Text LvlText => _LVLText;
@@ -20,5 +21,6 @@ namespace Code.TileSystem
         public TMP_Text UnitMax => _unitMax;
         public Button Upgrade => _upgrade;
         public Button Destroy => _destroy;
+        public TileResourcesView TileResourcesView => _tileResourcesView;
     }
 }
