1e34b161b923a5168ce8090b98b2dc0ba0e8aa80
LittiSecond <61295647+LittiSecond@users.noreply.github.com>
2023-05-01 13:36:47 +0300

    Added DefenderUiSlot prefab and WarsUiView

---
+++ Assets/Code/CombatSystem/Views/WarsUIView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace CombatSystem.Views
+{
+    public sealed class WarsUIView : MonoBehaviour
+    {
+        [field: SerializeField] public Button EnterToBarracks { get; private set; }
+        [field: SerializeField] public Transform[] Slots { get; private set; }
+        [field: SerializeField] public Sprite UnitDefenderSprite { get; private set; }
+        [field: SerializeField] public Sprite NoUnitSprite { get; private set; }
+    }
+}
