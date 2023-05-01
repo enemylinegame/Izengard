471c1bc756ad3bfb9a3dc088a0182be2a56bf50c
LittiSecond <61295647+LittiSecond@users.noreply.github.com>
2023-05-01 13:37:57 +0300

    Added WarsView and DefenderSlotView

---
+++ Assets/Code/CombatSystem/Views/WarsView.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+
+namespace CombatSystem.Views
+{
+    public sealed class WarsView
+    {
+        private const int FIRST_SLOT_NUMBER = 1;
+
+        private WarsUIView _warsUIView;
+        private DefenderSlotView[] _slots;
+
+
+        public WarsView(WarsUIView warsUIView)
+        {
+            _warsUIView = warsUIView;
+            CreateSlots();
+        }
+
+
+        private void CreateSlots()
+        {
+            Transform[] slotsTransforms = _warsUIView.Slots;
+
+            _slots = new DefenderSlotView[slotsTransforms.Length];
+
+            for (int i = 0; i < _slots.Length; i++)
+            {
+                DefenderSlotView newSlot = new DefenderSlotView(slotsTransforms[i], _warsUIView.UnitDefenderSprite,
+                    i + FIRST_SLOT_NUMBER);
+                newSlot.OnHireDissmisClick += HireDissmissButtonClick;
+                newSlot.OnInBarrackChanged += InBarrackToggleChanged;
+                _slots[i] = newSlot;
+            }
+
+        }
+
+        private void HireDissmissButtonClick(int slotNumber)
+        {
+
+        }
+
+        private void InBarrackToggleChanged(bool isOn, int slotNumber)
+        {
+
+        }
+
+
+    }
+}
