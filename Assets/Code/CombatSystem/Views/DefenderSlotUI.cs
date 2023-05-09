31ff81111d686292491573b30f8472240c977876
LittiSecond <61295647+LittiSecond@users.noreply.github.com>
2023-05-09 18:05:38 +0300

    Changed DefenderSlotUI and defender slot logick

--- Assets/Code/CombatSystem/Views/DefenderSlotUI.cs
+++ Assets/Code/CombatSystem/Views/DefenderSlotUI.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 
 namespace CombatSystem.Views
@@ -8,9 +9,11 @@ namespace CombatSystem.Views
     {
 
         [field: SerializeField] public Image UnitIcon { get; private set; }
+        [field: SerializeField] public Button UnitIconButton { get; private set; }
         [field: SerializeField] public Toggle SendToBarrackToggle { get; private set; }
         [field: SerializeField] public Button HireButton { get; private set; }
         [field: SerializeField] public Button DismissButton { get; private set; }
+        [field: SerializeField] public GameObject SelectBoard { get; private set; }
 
     }
 }
