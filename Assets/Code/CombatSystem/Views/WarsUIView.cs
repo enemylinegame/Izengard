57e24dbc0bbf0009aede852cc3abaccd48f2f300
LittiSecond <61295647+LittiSecond@users.noreply.github.com>
2023-05-11 13:17:58 +0300

    Added button exit from barrack

--- Assets/Code/CombatSystem/Views/WarsUIView.cs
+++ Assets/Code/CombatSystem/Views/WarsUIView.cs
@@ -7,6 +7,7 @@ namespace CombatSystem.Views
     public sealed class WarsUIView : MonoBehaviour
     {
         [field: SerializeField] public Button EnterToBarracks { get; private set; }
+        [field: SerializeField] public Button ExitFromBarracks { get; private set; }
         [field: SerializeField] public Button DismissButton { get; private set; }
         [field: SerializeField] public Button ToOtherTileButton { get; private set; }
         [field: SerializeField] public DefenderSlotUI[] Slots { get; private set; }
