a3a819766cc80e8d9240df244f6aea9a7abd501a
Александр Мельников <meln.alex97@gmail.com>
2023-06-26 22:39:02 +0700

    Created second version of marketManager and transfer files to folder
    Created MarketItem class which respond for market item logic.
    Created second version of Marketmanager with using MarketItem classes.
    Moved all Market classes in one folder

---
+++ Assets/Code/Market/MarketItem.cs
@@ -0,0 +1,55 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MarketItem : MonoBehaviour
+{
+    [Header("Amount Settings")]
+    [SerializeField] private int _initialAmount = 100;
+    [SerializeField] private int _minAmount = 50;
+    
+    [Header("Cost Settings")]
+    [SerializeField] private int _exchangeRate = 100;
+    
+    [Header("Item Texts Fields")]
+    [SerializeField] private TMP_Text _amountText;
+    [SerializeField] private TMP_Text _costText;
+
+    private int _exchangeAmount;
+    private int _currentAmount;
+
+    public int CurrentAmount => _currentAmount;
+    public int ExchangeCost => _exchangeAmount * _exchangeRate;
+
+    public void Init(int exchangeAmount)
+    {
+        _exchangeAmount = exchangeAmount;
+
+        _currentAmount = _initialAmount;
+        _costText.text = $"{_exchangeAmount} for {ExchangeCost} gold";
+        UpdateAmountText(_currentAmount);
+    }
+
+    public void IncreaseAmount(int amount)
+    {
+        _currentAmount -= amount;
+        
+        if(_currentAmount < _minAmount)
+        {
+            _currentAmount = _minAmount;
+        }
+
+        UpdateAmountText(_currentAmount);
+    }
+
+    public void DecreaseAmount(int amount) 
+    {
+        _currentAmount += amount;
+
+        UpdateAmountText(_currentAmount);
+    }
+
+    private void UpdateAmountText(int currentAmount)
+    {
+        _amountText.text = currentAmount.ToString();
+    }
+}
---
+++ Assets/Code/Market/MarketManagerV2.cs
@@ -0,0 +1,113 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public sealed class MarketManagerV2 : MonoBehaviour
+{
+    [Header("Castomer Settings")]
+    [SerializeField] private int _initialGold = 1000;
+    [SerializeField] private TMP_Text _goldText;
+    [SerializeField] private TMP_Text _statusText;
+
+    [Space(10)]
+    [Header("Market Items")]
+    [SerializeField] private MarketItem _woodItem;
+    [SerializeField] private MarketItem _ironItem;
+    [SerializeField] private int _exchangeTierOneAmount = 10;
+
+    [Space(10)]
+    [Header("Market Buttons")]
+    [SerializeField] private Button _buyWoodButton;
+    [SerializeField] private Button _buyIronButton;
+    [SerializeField] private Button _sellWoodButton;
+    [SerializeField] private Button _sellIronButton;
+
+
+    private int _currentGold;
+
+    private void Start()
+    {
+        _currentGold = _initialGold;
+
+        _woodItem.Init(_exchangeTierOneAmount);
+        _ironItem.Init(_exchangeTierOneAmount);
+
+        _buyWoodButton.onClick.AddListener(BuyWood);
+        _buyIronButton.onClick.AddListener(BuyIron);
+        _sellWoodButton.onClick.AddListener(SellWood);
+        _sellIronButton.onClick.AddListener(SellIron);
+
+        UpdateGoldText(_currentGold);
+        ChangeStatusText("");
+    }
+
+    private void BuyWood()
+    {
+        if (_currentGold >= _woodItem.ExchangeCost)
+        {
+            _woodItem.IncreaseAmount(_exchangeTierOneAmount);
+            _currentGold -= _woodItem.ExchangeCost;
+            UpdateGoldText(_currentGold);
+            ChangeStatusText("");
+        }
+        else
+        {
+            ChangeStatusText("Нужно больше золота");
+        }
+    }
+
+    private void BuyIron()
+    {
+        if (_currentGold >= _ironItem.ExchangeCost)
+        {
+            _ironItem.IncreaseAmount(_exchangeTierOneAmount);
+            _currentGold -= _ironItem.ExchangeCost;
+            UpdateGoldText(_currentGold);
+            ChangeStatusText("");
+        }
+        else
+        {
+            ChangeStatusText("Нужно больше золота");
+        }
+    }
+
+    private void SellWood()
+    {
+        if(_woodItem.CurrentAmount > _exchangeTierOneAmount)
+        {
+            _woodItem.DecreaseAmount(_exchangeTierOneAmount);
+            _currentGold += _woodItem.ExchangeCost;
+            UpdateGoldText(_currentGold);
+            ChangeStatusText("");
+        }
+        else
+        {
+            ChangeStatusText("Нужно больше древесины");
+        }
+    }
+
+    private void SellIron()
+    {
+        if (_ironItem.CurrentAmount > _exchangeTierOneAmount)
+        {
+            _ironItem.DecreaseAmount(_exchangeTierOneAmount);
+            _currentGold += _ironItem.ExchangeCost;
+            UpdateGoldText(_currentGold);
+            ChangeStatusText("");
+        }
+        else
+        {
+            ChangeStatusText("Нужно больше железа");
+        }
+    }
+
+    private void UpdateGoldText(int currentGold)
+    {
+        _goldText.text = currentGold.ToString();
+    }
+
+    private void ChangeStatusText(string text)
+    {
+        _statusText.text = text;
+    }
+}
