e6963d342743c6f8da56a36770e0d2bb980808c5
LittiSecond <61295647+LittiSecond@users.noreply.github.com>
2023-06-28 18:45:40 +0300

    Testing the collision dedecting

---
+++ Assets/Code/Tile System/OnTriggerDetector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.TileSystem
+{
+    public class OnTriggerDetector : MonoBehaviour
+    {
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            Debug.Log($"OnTriggerDetector->OnTriggerEnter: tr = {transform.name}; other = " +
+                      $"{other.gameObject.name} other layer = {other.gameObject.layer}");
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            Debug.Log($"OnTriggerDetector->OnTriggerExit: tr = {transform.name}; other = " +
+                      $"{other.gameObject.name} other layer = {other.gameObject.layer}");
+        }
+    }
+}
