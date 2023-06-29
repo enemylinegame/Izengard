c531a72eeed6ab38357ac0d4f1cb09c389c8452f
SouJl <meln.alex97@gmail.com>
2023-06-29 14:41:24 +0700

    GlobalStock implementation
    Impelemented GlobalStock in MarketController

--- Assets/Code/ResourceMarket/MVC/MarketController.cs
+++ Assets/Code/ResourceMarket/MVC/MarketController.cs
@@ -1,33 +1,35 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using ResourceSystem;
 
 namespace ResourceMarket
 {
     public sealed class MarketController
     {
-        private readonly Dictionary<ResourceType, MarketItemModel> _marketItems = new Dictionary<ResourceType, MarketItemModel>();
+        private readonly List<MarketItemModel> _marketItems = new List< MarketItemModel>();
 
-        private readonly ICustomer _customer;
+        private readonly GlobalStock _stock;
         private readonly IMarketDataProvider _marketDataProvider;
         private readonly MarketView _view;
+        private int _currentGold;
 
         public MarketController(
-            ICustomer customer,
-            IMarketDataProvider marketDataProvider,
             MarketView view,
-            MarketDataConfig marketData)
+            MarketDataConfig marketData,
+            GlobalStock stock,
+            IMarketDataProvider marketDataProvider)
         {
-            _customer = customer;
+            _stock = stock;
+            _stock.ResourceValueChanged += OnGoldChange;
+
             _marketDataProvider = marketDataProvider;
             _view = view;
 
             foreach(var itemData in marketData.MarketItemsData)
             {
-                _marketItems[itemData.ResourceType] = new MarketItemModel(itemData, _marketDataProvider);
+                _marketItems.Add(new MarketItemModel(itemData, _marketDataProvider));
             }
                
-            _view.InitView(_marketItems.Values.ToList(), OnByItem, OnSellItem);
+            _view.InitView(_marketItems, OnByItem, OnSellItem);
             
             _marketDataProvider.OnMarketAmountChange += _view.UpdateMarketAmount;
             _view.UpdateMarketAmount(_marketDataProvider.MarketAmount);
@@ -38,13 +40,15 @@ namespace ResourceMarket
             if (resourceType == ResourceType.None) 
                 return;
 
-            var item = _marketItems[resourceType];
+            var item = _marketItems.Find(r => r.ResourceType == resourceType);
 
-            if (_customer.Gold >= item.BuyCost)
+            if (_currentGold >= item.BuyCost)
             {
-                item.IncreaseAmount(item.ExchangeAmount);
-                _customer.RemoveGold(item.BuyCost);
-                _view.UpdateGold(_customer.Gold);
+                _stock.GetResourceFromStock(ResourceType.Gold, item.BuyCost);
+                _stock.AddResourceToStock(resourceType, item.ExchangeAmount);
+
+                item.DecreaseAmount(item.ExchangeAmount);
+
                 _view.UpdateStatus("");
             }
             else 
@@ -58,13 +62,15 @@ namespace ResourceMarket
             if (resourceType == ResourceType.None)
                 return;
 
-            var item = _marketItems[resourceType];
+            var item = _marketItems.Find(r => r.ResourceType == resourceType);
 
-            if (item.CurrentAmount > item.ExchangeAmount)
+            if (_stock.CheckResourceInStock(resourceType, item.ExchangeAmount) == false)
             {
-                item.DecreaseAmount(item.ExchangeAmount);
-                _customer.AddGold(item.ExchangeCost);
-                _view.UpdateGold(_customer.Gold);
+                _stock.GetResourceFromStock(resourceType, item.ExchangeAmount);
+                _stock.AddResourceToStock(ResourceType.Gold, item.ExchangeCost);
+
+                item.IncreaseAmount(item.ExchangeAmount);
+
                 _view.UpdateStatus("");
             }
             else
@@ -73,5 +79,13 @@ namespace ResourceMarket
             }
         }
 
+        private void OnGoldChange(ResourceType resourceType, int value)
+        {
+            if (resourceType != ResourceType.Gold) 
+                return;
+
+            _currentGold = value;
+            _view.UpdateGold(_currentGold);
+        }
     }
 }
--- Assets/Code/ResourceMarket/MarketEnetryPoint.cs
+++ Assets/Code/ResourceMarket/MarketEnetryPoint.cs
@@ -1,20 +1,51 @@
-﻿using UnityEngine;
+﻿using System;
+using ResourceSystem;
+using UnityEngine;
 
 namespace ResourceMarket
 {
     public sealed class MarketEnetryPoint : MonoBehaviour
     {
         [SerializeField] private MarketView _marketView;
+        [SerializeField] private GlobalResourceList _globalResourceList;
         [SerializeField] private MarketDataConfig _marketData;
         [SerializeField] private int _initialGold = 1000;
+        [SerializeField] private int _initialWood = 100;
+        [SerializeField] private int _initialIron = 100;
 
         private MarketController _marketController;
 
         private void Start()
         {
-            var customer = new TestCustomer(_initialGold);
+            var globalResStock = new GlobalStock(_globalResourceList.GlobalResourceConfigs);
             var marketProvider = new TestMarketDataProvider(_marketData.MarketCoef, _marketData.MarketBuildings);
-            _marketController = new MarketController(customer, marketProvider, _marketView, _marketData);
+            _marketController = new MarketController(_marketView, _marketData, globalResStock, marketProvider);
+
+            globalResStock.AddResourceToStock(ResourceType.Gold, _initialGold);
+            globalResStock.AddResourceToStock(ResourceType.Wood, _initialWood);
+            globalResStock.AddResourceToStock(ResourceType.Iron, _initialIron);
+
+            globalResStock.ResourceValueChanged += OnWoodChange;
+            globalResStock.ResourceValueChanged += OnIronChange;
+
         }
+
+        private void OnWoodChange(ResourceType resourceType, int value)
+        {
+            if (resourceType != ResourceType.Wood) 
+                return;
+
+            Debug.Log($"Wood in stock: {value}");
+        }
+
+
+        private void OnIronChange(ResourceType resourceType, int value)
+        {
+            if (resourceType != ResourceType.Iron)
+                return;
+
+            Debug.Log($"Iron in stock: {value}");
+        }
+
     }
 }
