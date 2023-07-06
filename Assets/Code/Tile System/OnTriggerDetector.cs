0587ba0395a718857dacd66774e62d7fb9309cda
LittiSecond <61295647+LittiSecond@users.noreply.github.com>
2023-07-06 07:16:42 +0300

    Added registration enemies into tile

--- Assets/Code/Tile System/OnTriggerDetector.cs
+++ Assets/Code/Tile System/OnTriggerDetector.cs
@@ -5,17 +5,22 @@ namespace Code.TileSystem
 {
     public class OnTriggerDetector : MonoBehaviour
     {
-        
+
+        public Action<GameObject> OnTriggerContactEnter;
+        public Action<GameObject> OnTriggerContactExit;
+
         private void OnTriggerEnter(Collider other)
         {
-            Debug.Log($"OnTriggerDetector->OnTriggerEnter: tr = {transform.name}; other = " +
-                      $"{other.gameObject.name}; other layer = {other.gameObject.layer}");
+            //Debug.Log($"OnTriggerDetector->OnTriggerEnter: tr = {transform.name}; other = " +
+            //          $"{other.gameObject.name}; other layer = {other.gameObject.layer}");
+            OnTriggerContactEnter?.Invoke(other.gameObject);
         }
 
         private void OnTriggerExit(Collider other)
         {
-            Debug.Log($"OnTriggerDetector->OnTriggerExit: tr = {transform.name}; other = " +
-                      $"{other.gameObject.name}; other layer = {other.gameObject.layer}");
+            //Debug.Log($"OnTriggerDetector->OnTriggerExit: tr = {transform.name}; other = " +
+            //          $"{other.gameObject.name}; other layer = {other.gameObject.layer}");
+            OnTriggerContactExit?.Invoke(other.gameObject);
         }
     }
 }
