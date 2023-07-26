ca2c6b502cc3e3541e38e927bd0cbc7417a166af
SouJl <meln.alex97@gmail.com>
2023-07-26 16:34:07 +0700

    Changed StartMenu scene presentation

--- Assets/Code/UI/SettingsMenu.cs
+++ Assets/Code/UI/SettingsMenu.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
-    public Dropdown resolutionDropdown;
+    public TMP_Dropdown resolutionDropdown;
 
     Resolution[] resolutions;
 
