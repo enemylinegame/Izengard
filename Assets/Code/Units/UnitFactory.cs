42b1f1e7c7590b6c1d00efc80ec5383e38779112
SouJl <meln.alex97@gmail.com>
2023-09-15 16:45:20 +0700

    Create unit factory class

---
+++ Assets/Code/Units/UnitFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Izengard.Units
+{
+    public class UnitFactory
+    {
+        public UnitFactory() { }
+
+        public IUnit CreateUnit(UnitFactionType factionType)
+        {
+            var unit = factionType switch
+            {
+                UnitFactionType.Enemy => CreateEnemy(),
+                UnitFactionType.Defender => CreateDefender(),
+                _=>null
+            };
+
+            return unit;
+        }
+
+        private IUnit CreateEnemy()
+        {
+            throw new NotImplementedException();
+        }
+
+        private IUnit CreateDefender()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
