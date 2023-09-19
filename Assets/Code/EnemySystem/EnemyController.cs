2251deec305d76cc3fd13ac2f6257c6375616f77
SouJl <meln.alex97@gmail.com>
2023-09-19 15:54:11 +0700

    Refactoring unitHandler and add enemy logic
    Refacrtored unitHandler view and model visiability;
    Added enemyController and enemyView;
    Implemented enemy test logic in level scene

---
+++ Assets/Code/EnemySystem/EnemyController.cs
@@ -0,0 +1,60 @@
+﻿using Izengard.UnitSystem;
+using UnityEngine;
+
+namespace Izengard.EnemySystem
+{
+    public class EnemyController : IOnController, IOnUpdate, IOnFixedUpdate
+    {
+        private readonly Vector3 _primaryTarget;
+        private readonly IUnit _unit;
+
+        private bool _isEnable;
+
+        public EnemyController(IUnit unit, Vector3 primaryTarget)
+        {
+            _unit = unit;
+            _primaryTarget = primaryTarget;
+
+            _isEnable = false;
+        }
+
+        public void Enable()
+        {
+            _isEnable = true;
+
+            _unit.Enable();
+
+            _unit.View.UnitNavigation.enabled = true;
+
+            if (_unit.View.UnitNavigation.isOnNavMesh)
+                _unit.View.UnitNavigation.ResetPath();
+
+            var unitPos = _unit.GetPosition();
+
+            _unit.View.UnitNavigation.Warp(unitPos);
+
+            MoveToTarget(_primaryTarget);
+        }
+
+
+        private void MoveToTarget(Vector3 target)
+        {
+            if (_unit.View.UnitNavigation.isOnNavMesh)
+            {
+                _unit.View.UnitNavigation.SetDestination(target);
+            }
+        }
+
+        public void OnUpdate(float deltaTime)
+        {
+            if (_isEnable == false)
+                return;
+        }
+
+        public void OnFixedUpdate(float fixedDeltaTime)
+        {
+            if (_isEnable == false)
+                return;
+        }
+    }
+}
---
+++ Assets/Code/UnitSystem/Interfaces/IUnitView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Izengard.UnitSystem
+{
+    public interface IUnitView
+    {
+        Transform SelfTransform { get; }
+        NavMeshAgent UnitNavigation { get; }
+
+        Animator UnitAnimator { get; }
+
+        void Show();
+        void Hide();
+        void ChangeHealth(int hpValue);
+        void ChangeSize(float sizeValue);
+        void ChangeArmor(int armorValue);
+        void ChangeSpeed(float speedValue);
+    }
+}
