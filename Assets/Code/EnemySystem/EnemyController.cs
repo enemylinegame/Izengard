9d147c409fe3cb731d7e73f9b878b3a0cafde627
SouJl <meln.alex97@gmail.com>
2023-09-20 14:15:12 +0700

    Add stop logic to enemyController

--- Assets/Code/EnemySystem/EnemyController.cs
+++ Assets/Code/EnemySystem/EnemyController.cs
@@ -8,14 +8,21 @@ namespace Izengard.EnemySystem
         private readonly Vector3 _primaryTarget;
         private readonly IUnit _unit;
 
+        private Vector3 _currentTarget;
+        private float _enemyStopDistance;
+
         private bool _isEnable;
+        private bool _isMove;
 
         public EnemyController(IUnit unit, Vector3 primaryTarget)
         {
             _unit = unit;
             _primaryTarget = primaryTarget;
 
+            _enemyStopDistance = _unit.Model.Offence.OffenceData.MeleeAttackReach;
+
             _isEnable = false;
+            _isMove = false;
         }
 
         public void Enable()
@@ -24,10 +31,12 @@ namespace Izengard.EnemySystem
 
             _unit.Enable();
 
+            _currentTarget = _primaryTarget;
+
             var navigationState = SetupNavigation();
             if(navigationState == true)
             {
-                MoveToTarget(_primaryTarget);
+                MoveToTarget(_currentTarget);
             }
         }
 
@@ -53,6 +62,7 @@ namespace Izengard.EnemySystem
             if (_unit.View.UnitNavigation.isOnNavMesh)
             {
                 _unit.View.UnitNavigation.SetDestination(target);
+                _isMove = true;
             }
         }
 
@@ -66,6 +76,33 @@ namespace Izengard.EnemySystem
         {
             if (_isEnable == false)
                 return;
+
+            if (_isMove)
+            {
+                if (CheckStopDistance(_currentTarget) == true)
+                {
+                    if (_unit.View.UnitNavigation.isOnNavMesh)
+                    {
+                        _unit.View.UnitNavigation.ResetPath();
+                        _isMove = false;
+                    }
+                }
+            }
         }
+
+        private bool CheckStopDistance(Vector3 position)
+        {
+            var distance = GetDistanceTo(position);
+            return distance < _enemyStopDistance;
+        }
+
+        public float GetDistanceTo(Vector3 targetPos)
+        {
+            var unitPosition = _unit.GetPosition();
+            var targetPosition = targetPos;
+
+            return Vector3.Distance(targetPosition, unitPosition);
+        }
+
     }
 }
