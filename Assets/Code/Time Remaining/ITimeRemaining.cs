85f78e1d19295f89c034a7609fe4580ec78cc54c
LittiSecond <breeze2882@yandex.ru>
2023-09-27 06:50:10 +0300

    Added TimeRemaning

---
+++ Assets/Code/Time Remaining/ITimeRemaining.cs
@@ -0,0 +1,9 @@
+﻿    public interface ITimeRemaining
+    {
+        bool IsRepeating { get; } 
+        float Duration { get; }
+        float TimeLeft { get; }
+        void Invoke();
+        void ChangeRemainingTime(float time);
+    }
+    
