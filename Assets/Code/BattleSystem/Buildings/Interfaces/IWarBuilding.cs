0659eb0185e516efb5770696ccf35a4e5aa9eb1b
LittiSecond <breeze2882@yandex.ru>
2023-10-29 20:59:15 +0300

    Add main tower prefab,WarBuilding view and handler

---
+++ Assets/Code/BattleSystem/Buildings/Interfaces/IWarBuilding.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+using Abstraction;
+using UnitSystem;
+
+
+namespace BattleSystem.Buildings.Interfaces
+{
+    public interface IWarBuilding : IDamageable
+    {
+        int Id { get; }
+        
+        IWarBuildingView View { get; }
+        
+        event Action<IWarBuilding> OnReachedZeroHealth;
+        
+        void Enable();
+
+        void Disable();
+    }
+}
---
+++ Assets/Code/BattleSystem/Buildings/Interfaces/IWarBuildingView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace BattleSystem.Buildings.Interfaces
+{
+    public interface IWarBuildingView
+    {
+        Vector3 Position { get; }
+        void Init(int id);
+        void Show();
+        void Hide();
+        void ChangeHealth(int hpValue);
+    }
+}
